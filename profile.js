8f51d526ce4dad5d7e883206691ef607bd9d5b97
isabel724 <isabel.l.serna@me.com>
2017-08-17 10:38:11 -0400

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,9 @@
+Accounts.onCreateUser(function(options, user) {
+    if (! options.profile) options.profile = {};
+    options.profile.artist = true;
+    options.profile.reputation = 100;
+    options.profile.someObject = {a: [], b: {}};
+
+    user.profile = options.profile;
+    return user;
+});
